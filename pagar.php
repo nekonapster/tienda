ca53ef03dd5809ae4633984fc9bb44911fc451e4
NekoNapster <mdmgrafica@gmail.com>
2023-02-24 16:36:58 +0100

    clave transaccion

--- pagar.php
+++ pagar.php
@@ -10,15 +10,30 @@ include 'templates/cabecera.php';
 <?php
 
     if ($_POST) {
-        $total=0;
+        $Total=0;
         $SID=session_id();
+        $Email=$_POST['email'];
+        
         foreach ($_SESSION['CARRITO'] as $indice => $producto) {
         
-            $total=$total+($producto['PRECIO']*$producto['CANTIDAD']);
+            $Total=$Total+($producto['PRECIO']*$producto['CANTIDAD']);
 
         
         }
-echo "<h3>".$total ."€" ."</h3>";
+        // insert en la tabla ventas de la base de datos en donde recojo la venta de los productos.
+        $sentencia=$pdo->prepare("INSERT INTO `ventas` 
+                            (`ID`, `ClaveTransaccio`, `PaypalDatos`, `Fecha`, `Email`, `Total`, `Status`) 
+        VALUES (NULL, :ClaveTransaccion, '', NOW(), :Email, :Total, 'pendiente');");
+
+        $sentencia->bindParam(":ClaveTransaccion", $SID);
+        $sentencia->bindParam(":Email", $Email);
+        $sentencia->bindParam(":Total", $Total);
+        $sentencia->execute();
+
+        // recolecto el ultimo id de las ventas
+        $idVentas=$pdo->lastInsertId();
+
+echo "<h3>".$Total ."€" ."</h3>";
 
     }
 
