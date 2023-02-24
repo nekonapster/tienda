b6c00e69dc4413b6bf24856f027e3d3f47ce3237
NekoNapster <mdmgrafica@gmail.com>
2023-02-24 15:15:45 +0100

    agregando campo de email en pagar

--- mostrarCarrito.php
+++ mostrarCarrito.php
@@ -12,52 +12,74 @@ include 'templates/cabecera.php';
 <?php
 if (!empty($_SESSION['CARRITO'])) { ?>
 
-<table class="table table-light table-bordered">
-    <tbody>
-        
-        <tr>
-            <th width="40%">Descripción</th>
-            <th width="15%" class="text-center" >Cantidad</th>
-            <th width="20%" class="text-center" >Precio</th>
-            <th width="20%" class="text-center" >Total</th>
-            <th width="5%"  class="text-center" >Accion</th>
-        </tr>
-        <?php $total=0;?>
+    <table class="table table-light table-bordered">
+        <tbody>
 
-        <?php foreach ($_SESSION['CARRITO'] as $indice=>$producto) { ?>
+            <tr>
+                <th width="40%">Descripción</th>
+                <th width="15%" class="text-center">Cantidad</th>
+                <th width="20%" class="text-center">Precio</th>
+                <th width="20%" class="text-center">Total</th>
+                <th width="5%" class="text-center">Accion</th>
+            </tr>
+            <?php $total = 0; ?>
 
-        <tr>
-            <td width="40%"> <?php echo $producto['RAZA'] ?> </td>
-            <td width="15%" class="text-center"><?php echo $producto['PRECIO'] ?></td>
-            <td width="20%" class="text-center"><?php echo $producto['CANTIDAD'] ?></td>
-            <td width="20%" class="text-center"><?php echo number_format($producto['PRECIO'] * $producto['CANTIDAD'],2); ?></td>
-            <td width="5%">
-            
-            <!-- formulario con boton para eliminar productos del carrito, recoge el id y lo encripta para luego compararlo asi borrarlo  -->
-            <form action="" method="post">
-                <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'], COD, KEY); ?>">
-                <button class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
-            </form>
-        
-    
-        </td>
-        </tr>
-        <?php $total=$total+ ($producto['PRECIO'] * $producto['CANTIDAD']) ;?>
-        <?php } ?>
+            <?php foreach ($_SESSION['CARRITO'] as $indice => $producto) { ?>
 
-        <tr>
-            <td colspan="3" align="right"> <h3>Total</h3> </td>
-            <td align="right"><h3> <?php echo number_format($total,2)?> €</h3></td>
-            <td></td>
-        </tr>    
+                <tr>
+                    <td width="40%"> <?php echo $producto['RAZA'] ?> </td>
+                    <td width="15%" class="text-center"><?php echo $producto['PRECIO'] ?></td>
+                    <td width="20%" class="text-center"><?php echo $producto['CANTIDAD'] ?></td>
+                    <td width="20%" class="text-center"><?php echo number_format($producto['PRECIO'] * $producto['CANTIDAD'], 2); ?></td>
+                    <td width="5%">
 
-    </tbody>
-</table>
+                        <!-- formulario con boton para eliminar productos del carrito, recoge el id y lo encripta para luego compararlo asi borrarlo  -->
+                        <form action="" method="post">
+                            <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'], COD, KEY); ?>">
+                            <button class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
+                        </form>
 
-<?php }else{ ?>
-<div class="alert alert-success" role="alert">
-    Carrito sin productos...
-</div>
+
+                    </td>
+                </tr>
+                <?php $total = $total + ($producto['PRECIO'] * $producto['CANTIDAD']); ?>
+            <?php } ?>
+
+            <tr>
+                <td colspan="3" align="right">
+                    <h3>Total</h3>
+                </td>
+                <td align="right">
+                    <h3> <?php echo number_format($total, 2) ?> €</h3>
+                </td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td colspan="5">
+                    <form action="pagar.php" method="post">
+                        <div class="alert alert-success" role="alert">
+                            <div class="form-group">
+                                <label for="my-input">Email de contacto: </label>
+                                <input id="email" name="email" class="form-control" type="email" placeholder="Porfavor escribe tu email" require>
+                            </div>
+                            <small id="emailHelp" class="form-text text-muted">
+                                Los productos se enviaran a este email
+                            </small>
+                        </div>
+                        <button class="btn btn-primary btn-lg btn-block" type="submit" value="pagar" name="btnAccion">Pagar >></button>
+                    </form>
+                </td>
+            </tr>
+
+
+        </tbody>
+    </table>
+
+<?php } else { ?>
+    <div class="alert alert-success" role="alert">
+        Carrito sin productos...
+    </div>
 <?php } ?>
 
 
