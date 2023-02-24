665eb0684dcd56b77fe0aa7cf12b5484d2ac058a
NekoNapster <mdmgrafica@gmail.com>
2023-02-24 12:36:11 +0100

    boton eliminar

--- carrito.php
+++ carrito.php
@@ -7,63 +7,90 @@ if (isset($_POST['btnAccion'])) {
 
     switch ($_POST['btnAccion']) {
 
-        case 'Agregar':
+        case "Agregar":
 
             if (is_numeric(openssl_decrypt($_POST['id'], COD, KEY))) {
                 $ID = openssl_decrypt($_POST['id'], COD, KEY);
-                $mensaje.= "OK ID: " . $ID."</br>";
+                $mensaje .= "OK ID: " . $ID . "</br>";
             } else {
-                $mensaje.= "UPS... ID inccorrecto " . $ID."</br>";
+                $mensaje .= "UPS... ID inccorrecto " . $ID . "</br>";
                 break;
             }
 
             if (is_string(openssl_decrypt($_POST['raza'], COD, KEY))) {
                 $RAZA = openssl_decrypt($_POST['raza'], COD, KEY);
-                $mensaje .= "OK raza: " . $RAZA."</br>";
-            } else { $mensaje .= "UPS... RAZA inccorrecta" . $RAZA."</br>"; break; }
+                $mensaje .= "OK raza: " . $RAZA . "</br>";
+            } else {
+                $mensaje .= "UPS... RAZA inccorrecta" . $RAZA . "</br>";
+                break;
+            }
 
             if (is_numeric(openssl_decrypt($_POST['precio'], COD, KEY))) {
                 $PRECIO = openssl_decrypt($_POST['precio'], COD, KEY);
-                $mensaje .= "OK precio: " . $PRECIO."</br>";
-            } else { $mensaje.= "UPS...PRECIO inccorrecto " . $PRECIO."</br>"; break; }
+                $mensaje .= "OK precio: " . $PRECIO . "</br>";
+            } else {
+                $mensaje .= "UPS...PRECIO inccorrecto " . $PRECIO . "</br>";
+                break;
+            }
 
             if (is_string(openssl_decrypt($_POST['cantidad'], COD, KEY))) {
                 $CANTIDAD = openssl_decrypt($_POST['cantidad'], COD, KEY);
-            
-                $mensaje .= "OK cantidad: " . $CANTIDAD."</br>";
-            } else { $mensaje.= "UPS... CANTIDAD inccorrecta " . $CANTIDAD."</br>";   break;}
-               
-            
+
+                $mensaje .= "OK cantidad: " . $CANTIDAD . "</br>";
+            } else {
+                $mensaje .= "UPS... CANTIDAD inccorrecta " . $CANTIDAD . "</br>";
+                break;
+            }
+
+
             // la forma de almacenar la informacion del carrito es mediante una sesion. sino existe recuperamos un array en donde volcaremos todos las variables del carrito de compra y la inicializamos en la posicion 0
-                if (!isset($_SESSION['CARRITO'])) {
-                    // declaro un array asociativo para meter en una variable los casos del switch
-                    $producto= [
-                            'ID'=>$ID,
-                            'RAZA'=>$RAZA,
-                            'PRECIO'=>$PRECIO,
-                            'CANTIDAD'=>$CANTIDAD,
-                                ];
-                                $_SESSION['CARRITO'][0]=$producto;
+            if (!isset($_SESSION['CARRITO'])) {
+                // declaro un array asociativo para meter en una variable los casos del switch
+                $producto = [
+                    'ID' => $ID,
+                    'RAZA' => $RAZA,
+                    'PRECIO' => $PRECIO,
+                    'CANTIDAD' => $CANTIDAD,
+                ];
+                $_SESSION['CARRITO'][0] = $producto;
 
-                                // forma de depositar mas productos en el carrito de compras
-                            }else {
-                                // cuento cuantos productos tengo en el carrito y lo almaceno en la variable $numeroProductos
-                                $numeroProductos= count($_SESSION['CARRITO']);
-                                $producto= [
-                                    // recolectamos la informacion de cada producto y lo almacenamos en el array $producto
-                                    'ID'=>$ID,
-                                    'RAZA'=>$RAZA,
-                                    'PRECIO'=>$PRECIO,
-                                    'CANTIDAD'=>$CANTIDAD,
-                                ];
-                                // en este caso si producto se inicializa en 1 con mumreroProductos iremos sumando cada producto.
-                                $_SESSION['CARRITO'][$numeroProductos]=$producto;
-                
-                }
-                $mensaje=print_r($_SESSION, true);
+                // forma de depositar mas productos en el carrito de compras
+            } else {
+                // cuento cuantos productos tengo en el carrito y lo almaceno en la variable $numeroProductos
+                $numeroProductos = count($_SESSION['CARRITO']);
+                $producto = [
+                    // recolectamos la informacion de cada producto y lo almacenamos en el array $producto
+                    'ID' => $ID,
+                    'RAZA' => $RAZA,
+                    'PRECIO' => $PRECIO,
+                    'CANTIDAD' => $CANTIDAD,
+                ];
+                // en este caso si producto se inicializa en 1 con mumreroProductos iremos sumando cada producto.
+                $_SESSION['CARRITO'][$numeroProductos] = $producto;
+            }
+            $mensaje = print_r($_SESSION, true);
+
+
+
+            break;    
+            case "Eliminar":
+                if(is_numeric(openssl_decrypt($_POST['id'], COD, KEY))) {
+                    $ID = openssl_decrypt($_POST['id'], COD, KEY);
             
+                    // evaluo los valores de todos los productos que coinciden con el indice para eliminarlo de la variable de sesion.
+                    foreach ($_SESSION['CARRITO'] as $indice=>$producto) {
+                        if ($producto['ID']==$ID) {
+                            unset($_SESSION['CARRITO'][$indice]);
+                            echo "<script>alert('Producto eliminado con exito');</script>";
+                        }
+
+                    }
 
 
-            break;
+                } else {
+                    $mensaje .= "UPS... ID inccorrecto " . $ID . "</br>";
+                }
+                break;
     }
 }
+
--- mostrarCarrito.php
+++ mostrarCarrito.php
@@ -31,7 +31,16 @@ if (!empty($_SESSION['CARRITO'])) { ?>
             <td width="15%" class="text-center"><?php echo $producto['PRECIO'] ?></td>
             <td width="20%" class="text-center"><?php echo $producto['CANTIDAD'] ?></td>
             <td width="20%" class="text-center"><?php echo number_format($producto['PRECIO'] * $producto['CANTIDAD'],2); ?></td>
-            <td width="5%"><button class="btn btn-danger" type="button">Eliminar</button></td>
+            <td width="5%">
+            
+            <!-- formulario con boton para eliminar productos del carrito, recoge el id y lo encripta para luego compararlo asi borrarlo  -->
+            <form action="" method="post">
+                <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'], COD, KEY); ?>">
+                <button class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
+            </form>
+        
+    
+        </td>
         </tr>
         <?php $total=$total+ ($producto['PRECIO'] * $producto['CANTIDAD']) ;?>
         <?php } ?>
